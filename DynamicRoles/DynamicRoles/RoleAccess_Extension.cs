c036432a3fe2c8a49121c62012311314b5c7ee49
Keith Richardson <36552751+kjrichardson@users.noreply.github.com>
2019-01-26 16:10:13 -0500

    Added extensions

---
+++ DynamicRoles/DynamicRoles/RoleAccess_Extension.cs
@@ -0,0 +1,14 @@
+using System;
+using PX.Objects;
+using PX.Data;
+using PX.SM;
+
+namespace DynamicRoles
+{
+  public class RoleAccess_Extension : PXGraphExtension<RoleAccess>
+  {
+    #region Event Handlers
+
+    #endregion
+  }
+}
